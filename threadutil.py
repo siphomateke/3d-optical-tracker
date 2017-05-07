4fcdf0820b3f5a8d9abf0204f766208508e8193f
nextgensparx <siphomateke@gmail.com>
2017-05-07 16:50:32 +0200

    Add base threading helper class

---
+++ threadutil.py
@@ -0,0 +1,29 @@
+import time
+from threading import Thread, ThreadError
+
+
+class ProgramThread:
+    def __init__(self, func):
+        self.func = func
+        self.thread_cancelled = False
+        self.thread = Thread(target=self._run)
+
+    def start_thread(self):
+        self.thread.start()
+
+    def _run(self):
+        while not self.thread_cancelled:
+            try:
+                self.func()
+            except ThreadError:
+                self.thread_cancelled = True
+
+    def is_running(self):
+        return self.thread.isAlive()
+
+    def shut_down_thread(self):
+        self.thread_cancelled = True
+        # block while waiting for thread to terminate
+        while self.thread.isAlive():
+            time.sleep(1)
+        return True
