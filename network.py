d19d6cfb964abd21ab6c26e2f18fc41f351aa406
nextgensparx <siphomateke@gmail.com>
2017-05-09 09:28:47 +0200

    Add output message buffer

--- network.py
+++ network.py
@@ -14,72 +14,60 @@ class NetworkSocket(ProgramThread):
         self.server_socket.bind(("", port))
         self.client_socket = None
         self.read_list = [self.server_socket]
+        self.write_list = [self.server_socket]
         self.open = False
 
-        self.x = 0
+        self.out_buffer = []
 
     def listen(self):
         self.server_socket.listen(5)
         print "Listening on port {}".format(self.port)
         self.start_thread()
 
+    def send(self, msg, header=""):
+        self.out_buffer.append((header, msg))
+
     def run(self):
-        readable, writable, errored = select.select(self.read_list, [], [])
+        # Fix network select
+        readable, writable, errored = select.select(self.read_list, self.write_list, [])
         for sock in readable:
             # On initial connection
             if sock is self.server_socket:
                 self.client_socket, address = self.server_socket.accept()
                 self.read_list.append(self.client_socket)
+                self.write_list.append(self.client_socket)
                 print "Connection from", address
                 self.open = True
 
-        if self.open:
-            y = math.sin(self.x)
-            z = math.cos(y)
-            self.x += 0.01
-            try:
-                self.client_socket.send('d:{},{},{}\n'.format(y, y, z))
-            except socket.error as err:
-                print "Socket error: {}".format(err)
-                self.open = False
+        for client in writable:
+            if len(self.out_buffer) > 0:
+                try:
+                    if len(self.out_buffer[0]) >= 2:
+                        combined_str = ""
+                        for i in xrange(len(self.out_buffer[0][1])):
+                            item = self.out_buffer[0][1][i]
+                            combined_str += "{}".format(item)
+                            if i < len(self.out_buffer[0][1]) - 1:
+                                combined_str += ","
+
+                        if len(combined_str) > 0:
+                            self.client_socket.send("{}{}\n".format(self.out_buffer[0][0], combined_str))
+                        del self.out_buffer[0]
+                except socket.error as err:
+                    print "Socket error: {}".format(err)
+                    self.write_list.remove(client)
+                    self.read_list.remove(client)
+                    self.open = False
 
     def close(self):
+        """
+        Terminates the network connection and waits for the thread to finish
+        """
         if self.open:
             self.client_socket.close()
         self.stop_thread()
 
 
-"""size = 1024
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setblocking(0)
-host = ""
-port = 35353
-server_socket.bind((host, port))
-
-server_socket.listen(5)
-print "Listening on port {}".format(port)
-
-read_list = [server_socket]
-x = 0
-client_socket = None
-while True:
-    readable, writable, errored = select.select(read_list, [], [])
-    for sock in readable:
-        # On initial connection
-        if sock is server_socket:
-            client_socket, address = server_socket.accept()
-            read_list.append(client_socket)
-            print "Connection from", address
-
-    if client_socket is not None:
-        y = math.sin(x)
-        z = math.cos(y)
-        x += 0.01
-        try:
-            client_socket.send('d:{},{},{}\n'.format(y, y, z))
-        except socket.error as err:
-            print "Socket error: {}".format(err)
-            client_socket = None"""
-
-net_socket = NetworkSocket()
-net_socket.listen()
+if __name__ == '__main__':
+    net_socket = NetworkSocket()
+    net_socket.listen()
