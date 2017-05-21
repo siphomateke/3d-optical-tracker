59d50cdc902286c07f5c3c15df30af74b0c730ed
nextgensparx <siphomateke@gmail.com>
2017-05-21 16:20:28 +0200

    Update to be compatible with new camera and marker classes

--- test3.py
+++ test3.py
@@ -1,16 +1,16 @@
 import cv2
 from camera_sensors import CameraSensors
-from camutils import Cam
+from camutils import IPCam
 import numpy as np
 import marker_utils
 
-ipcam_url = "http://192.168.8.100:8080/"
+ipcam_url = "http://192.168.8.103:8080/"
 
-cam = Cam(ipcam_url)
+cam = IPCam(ipcam_url)
 cam.start()
 
 
-class Marker:
+class MyMarker:
     def __init__(self, smoothing):
         self.loc = np.float64([0, 0, 0])
         self.loc_set = False
@@ -29,7 +29,7 @@ class Marker:
         self.loc += (self.dest - self.loc) / self.smoothing
 
 
-marker = Marker(4)
+marker = MyMarker(4)
 loc_mat = np.zeros((640, 1200, 3), np.uint8)
 img_size_set = False
 prevxd, prevyd = None, None
@@ -37,6 +37,7 @@ prev_set = False
 
 then = cv2.getTickCount()
 timer = 0
+marker_finder = marker_utils.MarkerFinder()
 while True:
     now = cv2.getTickCount()
     delta = ((now - then) / cv2.getTickFrequency())
@@ -44,20 +45,22 @@ while True:
 
     cam_found_marker = False
     x, y2 = None, None
-    if cam.is_opened():
-        frame = cam.get_frame()[:, ::-1, :]
+    if cam.frame_ready:
+        frame = cam.frame[:, ::-1, :]
         if not img_size_set:
             loc_mat = np.zeros(frame.shape, np.uint8)
             img_size_set = True
 
-        ret, xm, ym = marker_utils.find_marker(frame)
-        contour_mat = frame.copy()
-        cam_found_marker = ret
+        ret, markers = marker_finder.find_markers(frame)
         if ret:
-            x = xm / frame.shape[1]
-            y2 = ym / frame.shape[0]
-            cv2.circle(contour_mat, (int(xm), int(ym)), 10, (255, 0, 0), -1)
-            # cv2.imshow("Contour image2", contour_mat)
+            xm, ym = markers[0].pos[0], markers[0].pos[1]
+            contour_mat = frame.copy()
+            cam_found_marker = ret
+            if ret:
+                x = xm / frame.shape[1]
+                y2 = ym / frame.shape[0]
+                cv2.circle(contour_mat, (int(xm), int(ym)), 10, (255, 0, 0), -1)
+                # cv2.imshow("Contour image2", contour_mat)
 
     if cam_found_marker:
         # y = (y2 + y) / 2.0
@@ -74,13 +77,10 @@ while True:
         cv2.line(loc_mat, (xd, yd), (prevxd, prevyd), (255, 0, 0), 2)
         prevxd = xd
         prevyd = yd
-        # cv2.line(loc_mat, (xd, yd), (int(xd + ((roll / 90.0) * -400)), int(yd + ((pitch / 90.0) * 400))), (0, 255, 0),3)
+        # cv2.line(loc_mat, (xd, yd), (int(xd), int(yd), (0, 255, 0),3)
     else:
         timer += delta
-
-    if cam.is_opened():
-        cv2.imshow("Estimated position", cv2.addWeighted(loc_mat, 0.6, frame, 0.4, 0))
-    # cv2.imshow("Estimated position", resize_img(loc_mat, height=640))
+    cv2.imshow("win", loc_mat)
 
     if timer > 1:
         prev_set = False
@@ -95,5 +95,5 @@ while True:
     if key & 0xFF == ord('c'):
         loc_mat = np.zeros(frame.shape, np.uint8)
 
-cam.shut_down()
+cam.stop()
 cv2.destroyAllWindows()
